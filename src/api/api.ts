e867e3488e61bb0597cd48feb2ffb28988a369c9
Yauheni <eugene.kostroma@gmail.com>
2022-05-29 19:24:53 +0400

    fix: fixed passRecover in api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,17 +1,18 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: process.env.REACT_APP_BACK_URL || "http://localhost:7542/2.0/",
+  baseURL:
+    process.env.REACT_APP_BACK_URL || "https://neko-back.herokuapp.com/2.0",
   withCredentials: true,
 });
 
 const letter = {
-  email: "nya@nya.nya", // кому восстанавливать пароль
+  // кому восстанавливать пароль
   from: "test-front-admin <ai73a@yandex.by>",
   // можно указать разработчика фронта)
   message: `<div style="background-color: lime; padding: 15px">
 password recovery link: 
-<a href='http://localhost:3000/#/set-new-password/$token$'>
+<a href='http://localhost:3000/#/password-create/$token$'>
 link</a>
 </div>`, // хтмп-письмо, вместо $token$ бэк вставит токен
 };
@@ -47,8 +48,8 @@ export const authAPI = {
   editProfile(data: ProfileData) {
     return instance.put("/auth/me", data);
   },
-  passRecover() {
-    return instance.post("/auth/forgot", letter);
+  passRecover(email: string) {
+    return instance.post("/auth/forgot", { ...letter, email });
   },
   setNewPassword(data: PasswordResetData) {
     return instance.post("/auth/set-new-password", data);
