5366b9507a0416610b560dfb6e0d99f4124a9bbe
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2022-05-30 13:42:49 +0300

    fix: route path

--- src/constants/routePaths.ts
+++ src/constants/routePaths.ts
@@ -1,11 +1,11 @@
 export enum RoutePaths {
-  login = "login",
-  passwordCreate = "password-create",
-  passwordRecovery = "password-recovery",
-  profile = "profile",
-  registration = "registration",
-  testBench = "test-bench",
+  login = "/login",
+  passwordCreate = "/password-create/:token",
+  passwordRecovery = "/password-recovery",
+  profile = "/profile",
+  registration = "/registration",
+  testBench = "/test-bench",
   random = "*",
-  notFound = "404-not-found",
+  notFound = "/404-not-found",
   index = "/",
 }
