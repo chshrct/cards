026298f5939a9f50439b2a62912ef6d1647b3834
Irina Martsulevich <malinovayairiska@gmail.com>
2022-05-30 09:37:50 +0300

    feat: add layout for user card

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import UserCard from './UserCard/UserCard';
 
 const Profile = () => {
   return (
     <div>
-      <h2>Profile</h2>
+      <UserCard/>
     </div>
   );
 };
