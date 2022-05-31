e9b4b6555f65fc5389a1c9e1a74423e75e2b100b
Irina Martsulevich <malinovayairiska@gmail.com>
2022-05-31 13:07:56 +0300

    fix: styles for input, add floating label

--- src/components/shared/SuperInputText/SuperInputText.tsx
+++ src/components/shared/SuperInputText/SuperInputText.tsx
@@ -3,24 +3,26 @@ import React, {
   DetailedHTMLProps,
   InputHTMLAttributes,
   KeyboardEvent,
-} from "react";
-import s from "./SuperInputText.module.css";
+} from 'react'
+import s from './SuperInputText.module.css'
 
 // тип пропсов обычного инпута
 type DefaultInputPropsType = DetailedHTMLProps<
   InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
->;
+>
 
 // здесь мы говорим что у нашего инпута будут такие же пропсы как у обычного инпута
 // (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
 type SuperInputTextPropsType = DefaultInputPropsType & {
   // и + ещё пропсы которых нет в стандартном инпуте
-  onChangeText?: (value: string) => void;
-  onEnter?: () => void;
-  error?: string;
-  spanClassName?: string;
-};
+  onChangeText?: (value: string) => void
+  onEnter?: () => void
+  error?: string
+  spanClassName?: string
+  label?: string
+  id?: string
+}
 
 const SuperInputText: React.FC<SuperInputTextPropsType> = ({
   type, // достаём и игнорируем чтоб нельзя было задать другой тип инпута
@@ -31,38 +33,45 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = ({
   error,
   className,
   spanClassName,
+  label,
+  id,
   ...restProps // все остальные пропсы попадут в объект restProps
 }) => {
   const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-    onChange && // если есть пропс onChange
-      onChange(e); // то передать ему е (поскольку onChange не обязателен)
+    onChange && onChange(e) // если есть пропс onChange // то передать ему е (поскольку onChange не обязателен)
 
-    onChangeText && onChangeText(e.currentTarget.value);
-  };
+    onChangeText && onChangeText(e.currentTarget.value)
+  }
   const onKeyPressCallback = (e: KeyboardEvent<HTMLInputElement>) => {
-    onKeyPress && onKeyPress(e);
+    onKeyPress && onKeyPress(e)
 
     onEnter && // если есть пропс onEnter
-      e.key === "Enter" && // и если нажата кнопка Enter
-      onEnter(); // то вызвать его
-  };
+    e.key === 'Enter' && // и если нажата кнопка Enter
+      onEnter() // то вызвать его
+  }
 
-  const finalSpanClassName = `${s.error} ${spanClassName ? spanClassName : ""}`;
-  const finalInputClassName = `${error ? s.superInput + " " + s.errorInput : s.superInput
-    } ${className}`; // need to fix with (?:) and s.superInput
+  const finalSpanClassName: string = `${s.error} ${
+    spanClassName ? spanClassName : ''
+  }`
+  const finalInputClassName: string = `${
+    error ? s.superInput + ' ' + s.errorInput : s.superInput
+  } ${className}` // need to fix with (?:) and s.superInput
 
   return (
-    <>
+    <div className={s.superInputWrapper}>
       <input
-        type={"text"}
+        type={'text'}
         onChange={onChangeCallback}
         onKeyPress={onKeyPressCallback}
         className={finalInputClassName}
+        id={id}
+        required
         {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
       />
+      {label && <label htmlFor={id}>{label}</label>}
       {<span className={finalSpanClassName}>{error}</span>}
-    </>
-  );
-};
+    </div>
+  )
+}
 
-export default SuperInputText;
+export default SuperInputText
