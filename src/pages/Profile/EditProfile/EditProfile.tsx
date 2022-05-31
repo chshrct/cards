0997d3cb31aae8380ce63c87204202a0dae11105
Irina Martsulevich <malinovayairiska@gmail.com>
2022-05-31 13:10:33 +0300

    refactor: format code, add props to SuperInput

--- src/pages/Profile/EditProfile/EditProfile.tsx
+++ src/pages/Profile/EditProfile/EditProfile.tsx
@@ -1,29 +1,47 @@
-import React from 'react';
-import s from './EditProfile.module.css';
-import { useInput } from '../../../hooks/useInput/useInput'
+import React from 'react'
 import SuperInputText from '../../../components/shared/SuperInputText/SuperInputText'
+import { useInput } from '../../../hooks/useInput/useInput'
+import s from './EditProfile.module.css'
 
 const EditProfile: React.FC = () => {
-  const {value: nickName, handleInputValueChange: handleNickNameChange, error: errorNickName} = useInput();
-  const {value: email, handleInputValueChange: handleEmailChange, error: errorEmailName} = useInput();
+  const {
+    value: nickName,
+    handleInputValueChange: handleNickNameChange,
+    error: errorNickName,
+  } = useInput()
+  const {
+    value: email,
+    handleInputValueChange: handleEmailChange,
+    error: errorEmailName,
+  } = useInput()
 
   return (
-  <div className={s.container}>
-    <h2>Personal Information</h2>
-    <img src={'https://play-lh.googleusercontent.com/CWzqShf8hi-AhV9dUjzsqk2URzdIv8Vk2LmxBzf-Hc8T-oGkLVXe6pMpcXv36ofpvtc'} alt={''}/>
-        <SuperInputText
-                    value={nickName}
-                    onChangeText={handleNickNameChange}
-                    error={errorNickName}
-                />    
+    <div className={s.container}>
+      <h2>Personal Information</h2>
+      <img
+        src={
+          'https://play-lh.googleusercontent.com/CWzqShf8hi-AhV9dUjzsqk2URzdIv8Vk2LmxBzf-Hc8T-oGkLVXe6pMpcXv36ofpvtc'
+        }
+        alt={''}
+      />
 
       <SuperInputText
-                    value={email}
-                    onChangeText={handleEmailChange}
-                    error={errorEmailName}
-                />   
-  </div>
-  );
-};
+        value={nickName}
+        onChangeText={handleNickNameChange}
+        error={errorNickName}
+        label={'Nickname'}
+        id={'nickName'}
+      />
 
-export default EditProfile;
+      <SuperInputText
+        value={email}
+        onChangeText={handleEmailChange}
+        error={errorEmailName}
+        label={'Email'}
+        id={'email'}
+      />
+    </div>
+  )
+}
+
+export default EditProfile
