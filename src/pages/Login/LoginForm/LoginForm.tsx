7ae8ec2c64477ca9921134885501830d19761587
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2022-06-02 17:20:05 +0300

    fix: add useFormik

--- src/pages/Login/LoginForm/LoginForm.tsx
+++ src/pages/Login/LoginForm/LoginForm.tsx
@@ -1,101 +1,79 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { useFormik } from 'formik';
 import { NavLink } from 'react-router-dom';
 
-import { MAX_PASSWORD_LENGTH } from '../../../constant';
-import viewOp from '../../Registration/view.png';
-import viewCl from '../../Registration/viewClose.png';
+import { EMPTY_STRING } from '../../../constant';
+import { validateEmail, validatePassword } from '../../../helpers';
 import { loginUser } from '../loginReducer';
 
 import s from './LoginForm.module.css';
 
-import { SuperButton } from 'components';
+import {
+  SuperButton,
+  SuperCheckbox,
+  SuperInputPassword,
+  SuperInputText,
+} from 'components';
 import { AppRoutePaths } from 'routes';
 import { useAppDispatch } from 'store';
 
 export const LoginForm: React.FC = () => {
-  const viewOpen = {
-    backgroundImage: `url(${viewOp})`,
-  };
-
-  const viewClose = {
-    backgroundImage: `url(${viewCl})`,
-  };
-
-  const [view, setView] = useState(false);
-
   const dispatch = useAppDispatch();
-
-  const initialValues = { email: '', password: '', rememberMe: false };
-
-  const changeView = (): void => setView(!view);
-
-  const emailLength = 30;
+  const formik = useFormik({
+    initialValues: { email: '', password: '', rememberMe: false },
+    onSubmit: (values: any) => {
+      // eslint-disable-next-line no-debugger
+      debugger;
+      dispatch(loginUser(values.email, values.password, values.rememberMe));
+    },
+    validate: (values: any) => {
+      const errors = {} as any;
+      errors.email = validateEmail(values.email);
+      errors.password = validatePassword(values.password);
+      return errors;
+    },
+  });
+  const buttonDisabled =
+    !!formik.errors.email ||
+    !!formik.errors.password ||
+    formik.values.email === EMPTY_STRING ||
+    formik.values.password === EMPTY_STRING;
   return (
     <div>
-      <Formik
-        initialValues={initialValues}
-        validate={values => {
-          const errors = {} as { email: string; password: string };
-          if (!values.email) {
-            errors.email = 'Required';
-          } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-            errors.email = 'Invalid email address';
-          } else if (values.email.length >= emailLength) {
-            errors.email = 'Too long email';
-          }
-          if (!values.password) {
-            errors.password = 'Required';
-          } else if (values.password.length <= MAX_PASSWORD_LENGTH) {
-            errors.password = 'Must be longer than 7 characters';
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          dispatch(loginUser(values.email, values.password, values.rememberMe));
-          setSubmitting(false);
-        }}
-      >
-        {({ status }) => (
-          <Form className={s.formWrapper}>
-            <div>
-              <Field placeholder="login" type="email" name="email" className={s.input} />
-              <hr className={s.hr} />
-            </div>
-            <ErrorMessage name="email" component="div" className={s.errorMessage} />
-            <div className={s.showPassword}>
-              <Field
-                placeholder="password"
-                type={view ? 'text' : 'password'}
-                name="password"
-                className={s.input}
-              />
-              <div
-                role="none"
-                className={s.icon}
-                style={view ? viewOpen : viewClose}
-                onClick={changeView}
-              />
-            </div>
-            <hr className={s.hr} />
-            <ErrorMessage name="password" component="div" className={s.errorMessage} />
-            <div className={s.remember}>
-              <div>{status}</div>
-              <Field className={s.checkbox} type="checkbox" name="rememberMe" />
-              <span> remember me </span>
-            </div>
-            <div className={s.forgotPassword}>
-              <NavLink to={AppRoutePaths.PASSWORD_RECOVERY}>Forgot Password</NavLink>
-            </div>
-            <SuperButton type="submit" className={s.loginButton}>
-              Login
-            </SuperButton>
-            <span>Don`&apos;`t have an account?</span>
-            <NavLink to={AppRoutePaths.REGISTRATION}>Sign Up</NavLink>
-          </Form>
-        )}
-      </Formik>
+      <form onSubmit={formik.handleSubmit} className={s.formWrapper}>
+        <SuperInputText
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          name="email"
+          error={formik.errors.email}
+          label="Email"
+          id="Email"
+        />
+        <SuperInputPassword
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          placeholder="password"
+          name="password"
+          error={formik.errors.password}
+        />
+        <div className={s.remember}>
+          <SuperCheckbox
+            onChange={formik.handleChange}
+            checked={formik.values.rememberMe}
+            className={s.checkbox}
+            type="checkbox"
+            name="rememberMe"
+          />
+          <span> remember me </span>
+        </div>
+        <div className={s.forgotPassword}>
+          <NavLink to={AppRoutePaths.PASSWORD_RECOVERY}>Forgot Password</NavLink>
+        </div>
+        <SuperButton disabled={buttonDisabled}>Login</SuperButton>
+        <span>Don&apos;t have an account?</span>
+        <NavLink to={AppRoutePaths.REGISTRATION}>Sign Up</NavLink>
+      </form>
     </div>
   );
 };
