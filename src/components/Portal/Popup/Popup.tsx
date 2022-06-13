eab93e0af8047b2f61b59305da0e3046a9b065a8
Yauheni <eugene.kostroma@gmail.com>
2022-06-13 17:20:23 +0400

    fix: Popup controlled with onClose prop

--- src/components/Portal/Popup/Popup.tsx
+++ src/components/Portal/Popup/Popup.tsx
@@ -1,40 +1,27 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import { FC, ReactNode, useState } from 'react';
+import { FC, ReactNode } from 'react';
 
 import Portal from '../Portal';
 
 import s from './Popup.module.css';
 
-type PropsToSendType = {
-  closePopup: () => void;
-};
-
 type PropsType = {
-  children?: ((arg: PropsToSendType) => ReactNode) | ReactNode;
-  isOpened?: boolean;
+  children?: ReactNode;
+  isOpened: boolean;
+  onClose: () => void;
 };
 
-export const Popup: FC<PropsType> = ({ children, isOpened = true }) => {
-  const [isPopupOpened, setIsPopupOpened] = useState<boolean>(isOpened);
-
-  const closePopup = (): void => {
-    setIsPopupOpened(false);
-  };
-
-  const propsToSend: PropsToSendType = { closePopup };
-
-  if (!isPopupOpened) {
+export const Popup: FC<PropsType> = ({ children, isOpened = false, onClose }) => {
+  if (!isOpened) {
     return null;
   }
 
   return (
     <Portal>
       <div className={s.container} role="dialog">
-        <div className={s.overlay} role="button" tabIndex={0} onClick={closePopup} />
-        <div className={s.content}>
-          {typeof children === 'function' ? children(propsToSend) : children}
-        </div>
+        <div className={s.overlay} role="button" tabIndex={0} onClick={onClose} />
+        <div className={s.content}>{children}</div>
       </div>
     </Portal>
   );
