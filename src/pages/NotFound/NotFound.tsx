78200d7542af2424a2fe18c7444304ea9a0e42d1
Yauheni <eugene.kostroma@gmail.com>
2022-06-15 16:18:59 +0400

    fix. PageNotFound

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,21 @@
 import React, { FC } from 'react';
 
+import s from './NotFound.module.css';
+
 export const NotFound: FC = () => {
   return (
-    <div>
-      <h2>404 NotFound</h2>
+    <div className={s.container}>
+      <iframe
+        width="1120"
+        height="630"
+        src="https://www.youtube.com/embed/P5EG9A26tL8"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        // @ts-ignore
+        autoPlay
+      />
     </div>
   );
 };
